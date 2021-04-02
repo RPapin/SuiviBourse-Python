67d346813372dca419ea399c0c6cd5576072b723
RPapin <33548867+RPapin@users.noreply.github.com>
2021-04-02 11:48:05 +0200

    Add files via upload

--- api_app.py
+++ api_app.py
@@ -4,11 +4,14 @@ from os import path, listdir
 import os.path
 from os.path import isfile, join
 import getOldData as god
+from flask_cors import CORS
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 # app.config["DEBUG"] = True
 
 onlyfiles = [f for f in listdir(os.getcwd()) if isfile(join(os.getcwd(), f))]
 pathData = os.path.join(os.getcwd(), 'data.json')
+print(pathData)
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
