a9033817896bdcf2d0d821bc184cbae50711b9a0
RPapin <remi.papin@viacesi.fr>
2021-04-28 11:49:51 +0200

    data.json

--- fetchAndPublishData.bat
+++ fetchAndPublishData.bat
@@ -1,7 +1,7 @@
-start "py api_app.py" "test"
+start py api_app.py
 timeout 2
 start /wait curl http://localhost:5000/fetchData
 git add .
 git commit -m data.json
 git push 
-taskkill /im "test"
+exit
