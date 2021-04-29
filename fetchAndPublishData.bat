c1152422fc2dd09480bd5ab4a7a153b36c6ff989
RPapin <remi.papin@viacesi.fr>
2021-04-29 13:01:58 +0200

    auto push after fetch data

--- fetchAndPublishData.bat
+++ fetchAndPublishData.bat
@@ -2,7 +2,7 @@ start py api_app.py
 timeout 2
 start /wait curl http://localhost:5000/fetchData
 git add .
-git commit -m data.json
+git commit -m "auto push after fetch data"
 git push 
 taskkill /im py.exe
 exit
